ddc8f692be042c0cb7b55d257fb2c3028d504b71
fohalabi <ayomideabimbola79@gmail.com>
2025-09-08 22:34:39 +0100

    added product card components as a client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import ProductCard from "./components/ProductCard";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World!</h1>
+      <ProductCard />
     </main>
   );
 }
